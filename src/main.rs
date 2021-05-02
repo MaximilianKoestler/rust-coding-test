1f78211b66a2349139136ddd02a94bd9f0da1ae9
Maximilian Köstler <maximilian@koestler.dev>
2021-05-02 11:35:40 +0200

    Add unit test for main function core

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
 #![forbid(unsafe_code)]
 
-use pretty_env_logger;
 use anyhow::{anyhow, Result};
+use pretty_env_logger;
 
 use rust_coding_test::{
     csv_parser::iter_transactions, csv_writer::write_accounts,
     transaction_handler::TransactionHandler,
 };
+
+fn process_transactions(
+    source: impl std::io::Read,
+    destination: &mut dyn std::io::Write,
+) -> Result<()> {
+    let transactions = iter_transactions(source);
+
+    let mut handler = TransactionHandler::new();
+    handler.handle_transactions(transactions);
+
+    write_accounts(destination, handler.into_iter())
+}
 
 fn main() -> Result<()> {
     pretty_env_logger::init();
@@ -16,13 +28,39 @@ fn main() -> Result<()> {
         .ok_or_else(|| anyhow!("Missing input file"))?;
 
     let file = std::fs::File::open(path)?;
-    let transactions = iter_transactions(file);
-
-    let mut handler = TransactionHandler::new();
-    handler.handle_transactions(transactions);
-
     let mut stdout = Box::new(std::io::stdout());
-    write_accounts(&mut stdout, handler.into_iter())?;
+    process_transactions(file, &mut stdout)
+}
 
-    Ok(())
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_from_requirements() {
+        // Note: This test makes assumption about exact output format and order and is therefore
+        //       very brittle.
+        //       Its purpose is primarily to detect changes in the overall output format.
+
+        let source = br#"
+type, client, tx, amount
+deposit, 1, 1, 1.0
+deposit, 2, 2, 2.0
+deposit, 1, 3, 2.0
+withdrawal, 1, 4, 1.5
+withdrawal, 2, 5, 3.0
+"#;
+        let mut destination = vec![];
+
+        process_transactions(&source[..], &mut destination).unwrap();
+
+        let result = String::from_utf8(destination).unwrap();
+        assert_eq!(
+            &result,
+            r#"client,available,held,total,locked
+1,1.5,0,1.5,false
+2,2.0,0,2.0,false
+"#
+        );
+    }
 }
